bda178f02f67a42bb458aee18757073dd54fed13
BenRoe <mail@benjaminroesner.com>
2022-05-12 12:29:05 +0200

    Update NPM packages to latest, fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 // See https://tailwindcss.com/docs/configuration for details
 module.exports = {
-  purge: ["./src/**/*.js"],
+  content: ["./src/**/*.js"],
   // https://github.com/tailwindlabs/tailwindcss-forms
   plugins: [require("@tailwindcss/forms")],
 };
